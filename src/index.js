0151da69c3c408c1a56dc1e5a4324a858793528b
Antonio Berardi <antoberar@gmail.com>
2021-07-11 22:00:28 -0400

    Aprendiendo JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// const element = React.createElement('a',{href:'https://www.platzi.com/'},'Hola React!');
+let nombre = 'Manolo';
+// const element = React.createElement('h1',{},`Hola soy ${nombre}`);
+
+const jsx = <h1>Hola soy {2 + 2}</h1>
+const container = document.getElementById('root');
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+// ReactDOM.render(element,container);
+ReactDOM.render(jsx,container);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
