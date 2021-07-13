b5eba491cb71bc7606eea059552bc0fc5519036e
Antonio Berardi <antoberar@gmail.com>
2021-07-13 00:45:59 -0400

    Creando mi primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,31 @@
+import React from "react";
+import confLogo from "../images/badge-header.svg";
+import "./styles/Badge.css"; //se esta trayendo el .css pero no se asigna a ninguna variable.
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo Conferencia" />
+        </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="" alt="Avatar" />
+          <h1>
+            Antonio
+            <br />
+            Berardi
+          </h1>
+        </div>
+        <div className="Badge__section-info">
+          <p>Estudiante de Ingenieria</p>
+          <div>@anto9_berar5</div>
+        </div>
+
+        <div className="Badge__footer">Platzi Conf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+import Badge from "./components/Badge";
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-// const element = React.createElement('a',{href:'https://www.platzi.com/'},'Hola React!');
-let nombre = 'Manolo';
-// const element = React.createElement('h1',{},`Hola soy ${nombre}`);
+const container = document.getElementById("root");
 
-const jsx = <h1>Hola soy {2 + 2}</h1>
-const container = document.getElementById('root');
-
-/* ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-
-// ReactDOM.render(element,container);
-ReactDOM.render(jsx,container);
-
+ReactDOM.render(<Badge />, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
