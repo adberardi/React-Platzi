d84112bcaf29fde9393985c23cdea5001cd7aae9
Antonio Berardi <adberardi@yahoo.com>
2021-07-22 13:40:06 -0400

    Partes de un Badge

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import BadgeNew from "../pages/BadgeNew";
 import BadgeEdit from "../pages/BadgeEdit";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
+import BadgeDetails from '../pages/BadgeDetails';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           {/* <Route component={NotFound}/> */}
 {/*           <Route exact path="/404" component={NotFound} />
